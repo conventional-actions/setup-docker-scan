460980b240833d5f028f81bcccd74e7843cf1393
Anton Winter <anton@ketch.com>
2026-03-26 21:34:35 -0700

    feat!: Migrate to ESM, esbuild, and Node 24
    BREAKING CHANGE: Minimum runner must support node24.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,16 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {fileURLToPath} from 'url'
 import {test, expect} from '@jest/globals'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 test('test runs and fails gracefully without token', () => {
   process.env['RUNNER_TEMP'] = '/tmp'
   process.env['RUNNER_TOOL_CACHE'] = '/tmp'
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'dist', 'main', 'index.js')
+  const ip = path.join(__dirname, '..', 'dist', 'main', 'index.cjs')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
